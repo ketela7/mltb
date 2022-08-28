e8a0a03c75e3ad9697a0d4abe954dbf02fd778b0
ketela7 <ketela0137@gmail.com>
2022-08-28 15:36:47 +0700

    c

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -82,8 +82,9 @@ def _mirror_leech(bot, message, isZip=False, extract=False, isQbit=False, isLeec
         tag = message.from_user.mention_html(message.from_user.first_name)
 
     reply_to = message.reply_to_message
+    LOGGER.info(reply_to)
     if reply_to is not None:
-        file_ = reply_to.media# or reply_to.video or reply_to.audio or reply_to.photo or None
+        file_ = reply_to.document or reply_to.video or reply_to.audio or reply_to.photo or None
         if not reply_to.from_user.is_bot:
             if reply_to.from_user.username:
                 tag = f"@{reply_to.from_user.username}"
